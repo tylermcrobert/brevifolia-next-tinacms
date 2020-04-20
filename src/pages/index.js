f331348fcd68e499d2664f62135e0b7caff1736a
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2020-04-20 16:57:31 -0700

    Prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,72 +5,72 @@ import Layout from '../components/Layout'
 import BlogList from '../components/BlogList'
 
 const Index = ({ jsonFile, allBlogs }) => {
-	const formOptions = {
-		fields: [
-			{
-				name: 'title',
-				label: 'Site Title',
-				component: 'text'
-			},
-			{
-				name: 'description',
-				label: 'Site Description',
-				component: 'text'
-			},
-			{
-				name: 'repositoryUrl',
-				label: 'Repository Url',
-				component: 'text'
-			}
-		]
-	}
-	const [data] = useLocalJsonForm(jsonFile, formOptions)
+  const formOptions = {
+    fields: [
+      {
+        name: 'title',
+        label: 'Site Title',
+        component: 'text',
+      },
+      {
+        name: 'description',
+        label: 'Site Description',
+        component: 'text',
+      },
+      {
+        name: 'repositoryUrl',
+        label: 'Repository Url',
+        component: 'text',
+      },
+    ],
+  }
+  const [data] = useLocalJsonForm(jsonFile, formOptions)
 
-	return (
-		<Layout
-			pathname='/'
-			siteTitle={data.title}
-			siteDescription={data.description}
-		>
-			<section>
-				<BlogList allBlogs={allBlogs} />
-			</section>
-		</Layout>
-	)
+  return (
+    <Layout
+      pathname="/"
+      siteTitle={data.title}
+      siteDescription={data.description}
+    >
+      <section>
+        <BlogList allBlogs={allBlogs} />
+      </section>
+    </Layout>
+  )
 }
 
 export default Index
 
 Index.getInitialProps = async function() {
-	const content = await import(`../data/config.json`)
-	// get all blog data for list
-	const posts = (context => {
-		const keys = context.keys()
-		const values = keys.map(context)
-		const data = keys.map((key, index) => {
-			// Create slug from filename
-			const slug = key
-				.replace(/^.*[\\\/]/, '')
-				.split('.')
-				.slice(0, -1)
-				.join('.')
-			const value = values[index]
-			// Parse yaml metadata & markdownbody in document
-			const document = matter(value.default)
-			return {
-				document,
-				slug
-			}
-		})
-		return data
-	})(require.context('../posts', true, /\.md$/))
+  const content = await import(`../data/config.json`)
+  // get all blog data for list
+  const posts = (context => {
+    const keys = context.keys()
+    const values = keys.map(context)
+    const data = keys.map((key, index) => {
+      // Create slug from filename
+      const slug = key
+        .replace(/^.*[\\\/]/, '')
+        .split('.')
+        .slice(0, -1)
+        .join('.')
+      const value = values[index]
+      // Parse yaml metadata & markdownbody in document
+      const document = matter(value.default)
+      return {
+        document,
+        slug,
+      }
+    })
+    return data
+  })(require.context('../posts', true, /\.md$/))
 
-	return {
-		jsonFile: {
-			fileRelativePath: `src/data/config.json`,
-			data: content.default
-		},
+  return {
+    jsonFile: {
+      fileRelativePath: `src/data/config.json`,
+      data: content.default,
+    },
 
-		allBlogs: posts
-	}
+    allBlogs: posts,
+  }
 }
